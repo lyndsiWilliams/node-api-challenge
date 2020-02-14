c5fa80d74860587634a7c24f86785f0adf844ecb
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-14 13:36:31 -0600

    DELETE implemented to projectRouter

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -55,6 +55,20 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE - remove()
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(404).json({ message: "The project with the specified ID does not exist." });
+  } else {
+    Projects.remove(id).then(removed => {
+      res.status(200).json(removed);
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "The post's information could not be retrieved." })
+    });
+  };
+});
 
 
 module.exports = router;
