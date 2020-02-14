f164966520da31b778585f51adcd3563166be98c
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-14 12:20:25 -0600

    POST implemented to projectRouter

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -6,7 +6,7 @@ const Projects = require('./projectModel.js')
 const router = express.Router();
 
 
-// GET projects
+// GET projects - get()
 router.get('/', (req, res) => {
   console.log(Projects);
   Projects.get().then(projects => {
@@ -16,6 +16,28 @@ router.get('/', (req, res) => {
     res.status(500).json({ message: "Error retrieving projects" });
   });
 });
+
+// getProjectActions()
+
+// POST - insert()
+router.post('/', (req, res) => {
+  const projectData = req.body;
+
+  if (!projectData.name || !projectData.description) {
+    res.status(400).json({ message: "Please provide a name and description for this post." })
+  } else {
+    Projects.insert(projectData).then(post => {
+      res.status(201).json(post);
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "There was an error saving the post to the database." })
+    });
+  };
+});
+
+// PUT - update()
+
+// DELETE - remove()
 
 
 module.exports = router;
