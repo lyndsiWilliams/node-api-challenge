bcf0f700ea023b8bd67baeff84e7d0d666d21995
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-14 14:38:29 -0600

    PUT implemented on actionRouter

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -36,6 +36,17 @@ router.post('/', (req, res) => {
 });
 
 // PUT - update()
+router.put('/:id', (req, res) => {
+  const actionData = req.body;
+  const id = req.params.id;
+
+  Actions.update(id, actionData).then(action => {
+    res.status(200).json(action);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "There was an error updating action." })
+  });
+})
 
 // DELETE - remove()
 
